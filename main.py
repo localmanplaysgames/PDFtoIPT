5f2eaa9a3856e6a180549a672b2e048d5e3fb51a
localmanplaysgames <efitton5491@gmail.com>
2025-05-07 22:53:29 +0100

    about ready to start dumping into a txt

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 import camelot
-import pandas
+#import pandas
+from pypdf import PdfReader
 
 pdf_path = "./dev_resources/source.pdf"
 output_file = ""
 
+# count the number of pages in the provided pdf file
+def total_pages(pdf):
+    return len(PdfReader(pdf).pages)
+
+# turn the number of pages into a "n, n, n" string that camelot can understand
+def page_range(pdf):
+    return ",".join(str(i) for i in range(1, total_pages(pdf) + 1))
+
+# pass the pdf into camelot and get the tables
+def extract_tables(pdf):
+    return camelot.read_pdf(pdf, pages=page_range(pdf), flavor="lattice")
+
 def main():
 
-    print(f"running against pdf: {pdf_path}")
+    print(f"Running against pdf: {pdf_path}...")
 
-    pdf_pages = "6" # change this later to read the whole pdf
-    tables_extract = camelot.read_pdf(pdf_path, pages=pdf_pages, flavor="lattice")
-    
-    print(f"{tables_extract.n} tables found")
+    pages = total_pages(pdf_path)
+    print(f"Scanning {pages} pages for tables...")
+
+    tables = extract_tables(pdf_path)
+    print(f"{tables.n} tables found")
     print(f"Example of first table:")
-    print(tables_extract[0].df)
+    print(tables[0].df)
 
 if __name__ == __name__:
     main()
